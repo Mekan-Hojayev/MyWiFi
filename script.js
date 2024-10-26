97ce2bd072901086850ea3dc6de9870f3ce1960d
Mekan Hojayev <71170609+Mekan-Hojayev@users.noreply.github.com>
2024-10-26 06:37:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function generateWiFiQR(ssid, password, encryption) {
     });
 }
 
-var ssid = "smart";
-var password = "qawsedrf@2023";
+var ssid = "NetworkName";  // Replace with your WiFi SSID
+var password = "NetworkPassword";  // Replace with your WiFi Password
 var encryption = "WPA";  // Set to "WPA", "WEP", or "none" based on your encryption type
 
 generateWiFiQR(ssid, password, encryption);
